227ed6594b2ad9919379037daa6a56cbf5a6b10c
Joseph Furlott <jmfurlott@gmail.com>
2015-02-06 16:53:22 -0500

    semi-working MoreFromBlog but context error

--- src/shared/components/HTMLContentArea.js
+++ src/shared/components/HTMLContentArea.js
@@ -5,8 +5,13 @@ import chroma from 'chroma-js';
 import { color } from '../theme';
 import PLSlider from './PLSlider';
 import Video from './Video';
+import MoreFromBlog from './MoreFromBlog';
 
 let HTMLContentArea = React.createClass({
+
+  contextTypes: {
+    flux: React.PropTypes.any.isRequired,
+  },
 
   renderSliders() {
     // query selector
@@ -43,9 +48,33 @@ let HTMLContentArea = React.createClass({
     
   },
 
+  renderMoreFromBlog() {
+    let moreFromBlogs = Array.from(document.querySelectorAll('.more_from_blog'));
+    
+    let self = this;
+
+    moreFromBlogs.forEach(async function(moreFromBlog) { 
+      let slug1 = moreFromBlog.dataset.blog_1;
+      let slug2 = moreFromBlog.dataset.blog_2;
+
+      let PostActions = self.context.flux.getActions('posts');
+      await PostActions.getPostBySlug(slug1);
+      await PostActions.getPostBySlug(slug2);
+      
+      let PostStore = self.context.flux.getStore('posts');
+      let post1 = PostStore.getPostBySlug(slug1);
+      let post2 = PostStore.getPostBySlug(slug2);
+      
+      let posts = [post1, post2];
+      //console.log(posts); 
+      //React.render(<MoreFromBlog posts={posts} />, moreFromBlog);
+    });
+  },
+
   componentDidMount() {
     this.renderSliders();
     this.renderVideos();
+    this.renderMoreFromBlog();
   },
 
   render() {
